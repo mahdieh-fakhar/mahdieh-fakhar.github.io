58d6ee4266f03cfb28fdf93c4522155596a091f6
Mahdieh Fakhar <your-email@example.com>
2025-10-12 14:53:35 +0330

    Updated build configuration and files

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
+    outDir: path.resolve(import.meta.dirname, "."),
+    emptyOutDir: false,
   },
   server: {
     fs: {
